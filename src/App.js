9553dc8757bc9a090ef8d321ab64d2565be5b097
pennd44 <pdiba@oxbowschool.org>
2020-09-20 21:41:23 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // create your App component here
 import React from 'react';
-class App extends Component {
+class App extends React.Component {
     state = {  }
     componentDidMount(){
         fetch("http://api.open-notify.org/astros.json")
